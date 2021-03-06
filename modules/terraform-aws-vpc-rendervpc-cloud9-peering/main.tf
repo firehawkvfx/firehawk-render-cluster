f22c77126284bcfd04baf05a0357428ccf0fd6fc
Andrew Graham <andrew@firehawkvfx.com>
2021-03-06 17:30:42 +1030

    configure peering render_vpc

--- modules/terraform-aws-vpc-rendervpc-cloud9-peering/main.tf
+++ modules/terraform-aws-vpc-rendervpc-cloud9-peering/main.tf
@@ -3,37 +3,20 @@ provider "aws" {
   # in a dev environment these version locks below can be disabled.  in production, they should be locked based on the suggested versions from terraform init.
   version = "~> 3.15.0"
 }
-
-data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_canonical_user_id" "current" {}
-
 locals {
-  common_tags = {
-    environment  = var.environment
-    resourcetier = var.resourcetier
-    conflictkey  = var.conflictkey
-    # The conflict key defines a name space where duplicate resources in different deployments sharing this name are prevented from occuring.  This is used to prevent a new deployment overwriting and existing resource unless it is destroyed first.
-    # examples might be blue, green, dev1, dev2, dev3...dev100.  This allows us to lock deployments on some resources.
-    pipelineid = var.pipelineid
-    owner      = data.aws_canonical_user_id.current.display_name
-    accountid  = data.aws_caller_identity.current.account_id
-    region     = data.aws_region.current.name
-    terraform  = "true"
-  }
+  common_tags = merge(map("owner", data.aws_canonical_user_id.current.display_name), var.common_tags)
 }
-
-data "aws_vpc" "primary" { # The primary is the Main VPC containing vault
+data "aws_vpc" "primary" { # The primary is the VPC defined by the common tags var.  In this current instance - the render_vpc for the current resource tier.
   default = false
   tags    = local.common_tags
 }
-
 data "aws_vpc" "secondary" { # The secondary is the VPC containing the cloud 9 instance. 
   id = var.vpc_id_main_cloud9
 }
-
 resource "aws_vpc_peering_connection" "primary2secondary" {
-  vpc_id      = data.aws_vpc.primary.id   # Main VPC ID.
+  vpc_id      = data.aws_vpc.primary.id   # The render_vpc ID
   peer_vpc_id = data.aws_vpc.secondary.id # Secondary VPC ID.
   auto_accept = true                      # Flags that the peering connection should be automatically confirmed. This only works if both VPCs are owned by the same account.
 
@@ -42,35 +25,32 @@ resource "aws_vpc_peering_connection" "primary2secondary" {
   # # https://www.terraform.io/docs/providers/aws/d/caller_identity.html
   # peer_owner_id = "${data.aws_caller_identity.current.account_id}"
 }
-
 data "aws_route_table" "main_private" {
   tags = {
     "conflictkey" : local.common_tags["conflictkey"],
     "pipelineid" : local.common_tags["pipelineid"],
+    "vpcname" : local.common_tags["vpcname"],
     "area" : "private",
   }
 }
-
 data "aws_route_table" "main_public" {
   tags = {
     "conflictkey" : local.common_tags["conflictkey"],
     "pipelineid" : local.common_tags["pipelineid"],
+    "vpcname" : local.common_tags["vpcname"],
     "area" : "public",
   }
 }
-
 resource "aws_route" "primaryprivate2secondary" {
   route_table_id            = data.aws_route_table.main_private.id
   destination_cidr_block    = data.aws_vpc.secondary.cidr_block               # CIDR block / IP range for VPC 2.
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2secondary.id # ID of VPC peering connection.
 }
-
 resource "aws_route" "primarypublic2secondary" {
   route_table_id            = data.aws_route_table.main_public.id
   destination_cidr_block    = data.aws_vpc.secondary.cidr_block               # CIDR block / IP range for VPC 2.
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2secondary.id # ID of VPC peering connection.
 }
-
 resource "aws_route" "secondary2primary" {
   route_table_id            = data.aws_vpc.secondary.main_route_table_id      # ID of VPC 2 main route table.
   destination_cidr_block    = data.aws_vpc.primary.cidr_block                 # CIDR block / IP range for VPC 2.
--- modules/terraform-aws-vpc-rendervpc-cloud9-peering/variables.tf
+++ modules/terraform-aws-vpc-rendervpc-cloud9-peering/variables.tf
@@ -5,7 +5,7 @@ variable "sleep" {
 }
 variable "vpc_id_main_cloud9" {
   description = "The VPC ID containing the cloud9 seed instance in your main account.  This will be used to establish VPC peering with vault."
-  type = string
+  type        = string
 }
 variable "environment" {
   description = "The environment.  eg: dev/prod"
@@ -20,6 +20,11 @@ variable "pipelineid" {
   type        = string
 }
 variable "conflictkey" {
-    description = "The conflictkey is a unique name for each deployement usuallly consisting of the resourcetier and the pipeid."
-    type = string
+  description = "The conflictkey is a unique name for each deployement usuallly consisting of the resourcetier and the pipeid."
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags for all resources in a deployment run."
+  type        = map(string)
 }
