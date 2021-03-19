4e206fe6d0c38828bc5d25849f1eebdef548df74
Andrew Graham <andrew@firehawkvfx.com>
2021-03-20 10:18:42 +1030

    add deadline profile and find images

--- update_vars.sh
+++ update_vars.sh
@@ -127,22 +127,22 @@ fi
 # AMI query by commit - Vault and Consul Servers
 export TF_VAR_ami_commit_hash="$(cd $TF_VAR_firehawk_path/../packer-firehawk-amis/modules/firehawk-ami; git rev-parse HEAD)" 
 
-# AMI query by commit - Vault and Consul Server
-ami_role="firehawk_ubuntu18_vault_consul_server_ami"
-export TF_VAR_vault_consul_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-warn_if_invalid "$ami_role" "$TF_VAR_vault_consul_ami_id" "TF_VAR_vault_consul_ami_id"
-# AMI query by commit - Vault and Consul Client
-ami_role="firehawk_centos7_ami"
-export TF_VAR_vault_client_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-warn_if_invalid "$ami_role" "$TF_VAR_vault_client_ami_id" "TF_VAR_vault_client_ami_id"
-# AMI query by commit - Bastion Host
-ami_role="firehawk_centos7_ami"
-export TF_VAR_bastion_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-warn_if_invalid "$ami_role" "$TF_VAR_bastion_ami_id" "TF_VAR_bastion_ami_id"
-# AMI query by commit - Open VPN Server
-ami_role="firehawk_openvpn_server_ami"
-export TF_VAR_openvpn_server_ami=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
-warn_if_invalid "$ami_role" "$TF_VAR_openvpn_server_ami" "TF_VAR_openvpn_server_ami"
+# AMI query by commit - Deadline DB
+ami_role="firehawk_deadlinedb_ami"
+export TF_VAR_deadline_db_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+warn_if_invalid "$ami_role" "$TF_VAR_deadline_db_ami_id" "TF_VAR_deadline_db_ami_id"
+# AMI query by commit - Render node
+ami_role="firehawk_centos7_rendernode_ami"
+export TF_VAR_centos7_rendernode_ami=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+warn_if_invalid "$ami_role" "$TF_VAR_centos7_rendernode_ami" "TF_VAR_centos7_rendernode_ami"
+# # AMI query by commit - Bastion Host
+# ami_role="firehawk_centos7_ami"
+# export TF_VAR_bastion_ami_id=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+# warn_if_invalid "$ami_role" "$TF_VAR_bastion_ami_id" "TF_VAR_bastion_ami_id"
+# # AMI query by commit - Open VPN Server
+# ami_role="firehawk_openvpn_server_ami"
+# export TF_VAR_openvpn_server_ami=$(retrieve_ami $latest_ami $ami_role $TF_VAR_ami_commit_hash)
+# warn_if_invalid "$ami_role" "$TF_VAR_openvpn_server_ami" "TF_VAR_openvpn_server_ami"
 
 # Terraform Vars
 export TF_VAR_general_use_ssh_key="$HOME/.ssh/id_rsa" # For debugging deployment of most resources- not for production use.
