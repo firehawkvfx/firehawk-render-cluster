bcb6995375aa9a1f677bfde1d23197e072cbc3f7
Andrew Graham <andrew@firehawkvfx.com>
2021-04-11 13:30:02 +0930

    remove ref to instance id

--- modules/terraform-aws-render-vpc-routes/main.tf
+++ modules/terraform-aws-render-vpc-routes/main.tf
@@ -10,51 +10,34 @@ data "aws_route_tables" "public" {
   vpc_id = data.aws_vpc.primary.id
   tags   = merge(var.common_tags_rendervpc, { "area" : "public" })
 }
-
 data "aws_route_tables" "private" {
   vpc_id = data.aws_vpc.primary.id
   tags   = merge(var.common_tags_rendervpc, { "area" : "private" })
 }
-
-data "aws_instance" "vpn" {
-  instance_tags = merge(var.common_tags_vaultvpc, { "role" : "vpn" })
-}
-
 data "aws_vpc_peering_connection" "primary2secondary" {
   vpc_id = data.aws_vpc.primary.id
   peer_vpc_id = data.aws_vpc.secondary.id
   tags = merge( var.common_tags_rendervpc, { "peer_to" : "vault" } )
 }
-
 locals {
   private_route_table_ids = sort(data.aws_route_tables.private.ids)
   public_route_table_ids  = sort(data.aws_route_tables.public.ids)
-  id                      = data.aws_instance.vpn.id
 }
-
 # Route tables to send traffic to the remote subnet are configured once the vpn is provisioned.
-
 resource "aws_route" "private_openvpn_remote_subnet_gateway" {
   count = length(local.private_route_table_ids)
-
   route_table_id         = element(concat(local.private_route_table_ids, list("")), count.index)
   destination_cidr_block = var.onsite_private_subnet_cidr
-  # instance_id            = local.id
   vpc_peering_connection_id = data.aws_vpc_peering_connection.primary2secondary.id
-
   timeouts {
     create = "5m"
   }
 }
-
 resource "aws_route" "public_openvpn_remote_subnet_gateway" {
   count = length(local.public_route_table_ids)
-
   route_table_id         = element(concat(local.public_route_table_ids, list("")), count.index)
   destination_cidr_block = var.onsite_private_subnet_cidr
-  # instance_id            = local.id
   vpc_peering_connection_id = data.aws_vpc_peering_connection.primary2secondary.id
-
   timeouts {
     create = "5m"
   }
@@ -63,25 +46,18 @@ resource "aws_route" "public_openvpn_remote_subnet_gateway" {
 ### routes may be needed for traffic going back to open vpn dhcp adresses
 resource "aws_route" "private_openvpn_remote_subnet_vpndhcp_gateway" {
   count = length(local.private_route_table_ids)
-
   route_table_id         = element(concat(local.private_route_table_ids, list("")), count.index)
   destination_cidr_block = var.vpn_cidr
-  # instance_id            = local.id
   vpc_peering_connection_id = data.aws_vpc_peering_connection.primary2secondary.id
-
   timeouts {
     create = "5m"
   }
 }
-
 resource "aws_route" "public_openvpn_remote_subnet_vpndhcp_gateway" {
   count = length(local.public_route_table_ids)
-
   route_table_id         = element(concat(local.public_route_table_ids, list("")), count.index)
   destination_cidr_block = var.vpn_cidr
-  # instance_id            = local.id
   vpc_peering_connection_id = data.aws_vpc_peering_connection.primary2secondary.id
-
   timeouts {
     create = "5m"
   }
