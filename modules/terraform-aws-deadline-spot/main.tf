0b012eb5000e64f91a57e57077a3d773c50cc1dc
Andrew Graham <andrew@firehawkvfx.com>
2021-04-25 19:41:59 +0930

    update ansible playbook

--- modules/terraform-aws-deadline-spot/main.tf
+++ modules/terraform-aws-deadline-spot/main.tf
@@ -1,29 +1,97 @@
 # This module will configure Deadline's Spot event plugin to scale render nodes based on the queue for deadline groups
 
+data "aws_region" "current" {}
+data "aws_ami" "rendernode" {
+  most_recent = true
+  # If we change the AWS Account in which test are run, update this value.
+  owners = ["562637147889"]
+  filter {
+    name   = "tag:ami_role"
+    values = ["firehawk_centos7_rendernode_ami"]
+  }
+  filter {
+    name   = "tag:commit_hash"
+    values = [var.ami_commit_hash]
+  }
+  filter {
+    name   = "owners"
+    values = ["self"]
+  }
+}
+data "aws_vpc" "rendervpc" {
+  default = false
+  tags    = var.common_tags_rendervpc
+}
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.rendervpc.id
+  tags   = map("area", "private")
+}
+data "terraform_remote_state" "rendernode_profile" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+  backend = "s3"
+  config = {
+    bucket = "state.terraform.${var.bucket_extension_vault}"
+    key    = "firehawk-main/modules/terraform-aws-iam-profile-rendernode/terraform.tfstate"
+    region = data.aws_region.current.name
+  }
+}
+data "terraform_remote_state" "rendernode_security_group" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+  backend = "s3"
+  config = {
+    bucket = "state.terraform.${var.bucket_extension}"
+    key    = "firehawk-render-cluster/modules/terraform-aws-sg-rendernode/module/terraform.tfstate"
+    region = data.aws_region.current.name
+  }
+}
+
+locals "snapshot_id" {
+  ami_id                             = data.aws_ami.rendernode.id
+  snapshot_id                        = data.aws_ami.rendernode.id.block_device_mappings["/dev/sda1"].snapshot_id
+  private_subnet_ids                 = tolist(data.aws_subnet_ids.private.ids)
+  instance_profile                   = data.terraform_remote_state.rendernode_profile.outputs.instance_profile_arn
+  security_group_id                  = data.terraform_remote_state.rendernode_security_group.outputs.security_group_id
+  config_template_file_path          = "${path.module}/ansible/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/files/config_template.json"
+  override_config_template_file_path = "/home/ec2-user/config_template.json"
+}
 resource "null_resource" "provision_deadline_spot" {
   count      = (var.aws_nodes_enabled && var.provision_deadline_spot_plugin) ? 1 : 0
   depends_on = [null_resource.dependency_deadline_spot, module.node.ami_id, module.firehawk_init.local-provisioning-complete]
 
   triggers = {
     ami_id                  = module.node.ami_id
-    config_template_sha1    = sha1(file( fileexists(local.override_config_template_file_path) ? local.override_config_template_file_path : local.config_template_file_path))
-    deadline_spot_sha1      = sha1(file("/deployuser/ansible/ansible_collections/firehawkvfx/deadline/deadline_spot.yaml"))
-    deadline_spot_role_sha1 = sha1(file("/deployuser/ansible/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/tasks/main.yml"))
-    deadline_roles_tf_sha1  = sha1(file("/deployuser/modules/deadline/main.tf"))
-    spot_access_key_id      = module.deadline.spot_access_key_id
-    spot_secret             = module.deadline.spot_secret
-    volume_size             = var.node_centos_volume_size
-    volume_type             = var.node_centos_volume_type
+    config_template_sha1    = sha1(file(fileexists(local.override_config_template_file_path) ? local.override_config_template_file_path : local.config_template_file_path))
+    deadline_spot_sha1      = sha1(file("${path.module}/ansible/ansible_collections/firehawkvfx/deadline/deadline_spot.yaml"))
+    deadline_spot_role_sha1 = sha1(file("${path.module}/ansible/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/tasks/main.yml"))
+    deadline_roles_tf_sha1  = sha1(file("${path.module}/modules/deadline/main.tf"))
+    # spot_access_key_id      = module.deadline.spot_access_key_id
+    # spot_secret             = module.deadline.spot_secret
+    volume_size = var.node_centos_volume_size
+    volume_type = var.node_centos_volume_type
   }
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command = <<EOT
+    command     = <<EOT
       export SHOWCOMMANDS=true; set -x
-      cd /deployuser
-      echo ${module.deadline.spot_access_key_id}
-      echo ${module.deadline.spot_secret}
-      ANSIBLE_STDOUT_CALLBACK=debug ansible-playbook -i "$TF_VAR_inventory" ansible/ansible_collections/firehawkvfx/deadline/deadline_spot.yaml -v --extra-vars 'volume_type=${var.node_centos_volume_type} volume_size=${var.node_centos_volume_size} ami_id=${module.node.ami_id} snapshot_id=${module.node.snapshot_id} subnet_id=${module.vpc.private_subnets[0]} spot_instance_profile_arn="${module.deadline.spot_instance_profile_arn}" security_group_id=${module.node.security_group_id} spot_access_key_id=${module.deadline.spot_access_key_id} spot_secret=${module.deadline.spot_secret} account_id=${lookup(local.common_tags, "accountid", "0")}'
+      echo "Ensure SSH Certs are configured correctly with the current instance for the Ansible playbook to configure Deadline Spot Plugin"
+      cd ${path.module}/ansible
+      printf "\n...Waiting for consul deadlinedb service before attempting to configure.\n\n"
+      until consul catalog services | grep -m 1 "deadlinedb"; do sleep 1 ; done
+      set -x
+      ANSIBLE_STDOUT_CALLBACK=debug ansible-playbook -i "${path.module}/ansible/inventory/hosts" ansible/ansible_collections/firehawkvfx/deadline/deadline_spot.yaml -v --extra-vars "\
+        config_generated_json=\"$HOME/config_generated.json\" \
+        max_spot_capacity_engine=1 \
+        max_spot_capacity_mantra=1 \
+        volume_type=${var.node_centos_volume_type} \
+        volume_size=${var.node_centos_volume_size} \
+        ami_id=${local.ami_id} \
+        snapshot_id=${local.snapshot_id} \
+        subnet_id=${local.private_subnet_ids[0]} \
+        spot_instance_profile_arn=\"${local.instance_profile}\" \
+        security_group_id=${local.security_group_id} \
+        aws_region=${data.aws_region.current.name} \
+        aws_key_name=${var.aws_key_name} \
+        account_id=${lookup(local.common_tags, "accountid", "0")} \
+        "
 EOT
   }
 }
---
+++ modules/terraform-aws-deadline-spot/variables.tf
@@ -0,0 +1,27 @@
+variable "common_tags" {
+  description = "Common tags for all resources in a deployment run."
+  type        = map(string)
+}
+variable "common_tags_vaultvpc" {
+  description = "Common tags for resources in the vault vpc / firehawk-main project."
+  type        = map(string)
+}
+variable "common_tags_rendervpc" {
+  description = "Common tags for resources in the render vpc / firehawk-render-cluster project."
+  type        = map(string)
+}
+variable "node_centos_volume_size" {
+  default = 20
+}
+variable "node_centos_volume_type" {
+  default = "gp2"
+}
+variable "ami_commit_hash" {
+  description = "The commit hash tagged for the ami"
+  type        = string
+}
+variable "aws_key_name" {
+  description = "The name of the AWS PEM key for access to the instance"
+  type        = string
+  default     = null
+}
