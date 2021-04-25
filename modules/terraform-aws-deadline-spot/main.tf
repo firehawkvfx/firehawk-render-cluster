d9c039cfe75497755b20fcc7512dcbab67da0ec2
Andrew Graham <andrew@firehawkvfx.com>
2021-04-25 21:05:46 +0930

    get snapshot id

--- modules/terraform-aws-deadline-spot/main.tf
+++ modules/terraform-aws-deadline-spot/main.tf
@@ -42,6 +42,34 @@ output "block_device_mappings" {
   value = data.aws_ami.rendernode.block_device_mappings
 }
 
+variable "ebs_empty_map" {
+  type = map(string)
+  default = {
+    device_name = "/dev/sda1"
+    snapshot_id = ""
+  }
+}
+# We create a list with a dummy map as the 2nd / last element.
+locals {
+  ebs_block_device_extended = concat(data.aws_ami.rendernode.block_device_mappings, list(list(var.ebs_empty_map)))
+  # We select the first element in the list.  if the actual node exists, we will eventually get a valid value after the for loop below, otherwise it will return blank from the empty map, which is fine, since the ami id should never be referenced in this state.
+  ebs_block_device_selected = element(local.ebs_block_device_extended, 0)
+}
+
+# This loop creates key's based on the device name, so the snapshot_id can be retrieved by the device name.
+locals {
+  ebs_block_device = {
+    for bd in local.ebs_block_device_selected :
+    bd.device_name => bd
+  }
+}
+
+output "snapshot_id" {
+  value = local.ebs_block_device["/dev/sda1"].snapshot_id
+}
+
+
+
 locals {
   ami_id = data.aws_ami.rendernode.id
   # snapshot_id                        = data.aws_ami.rendernode.id.block_device_mappings["/dev/sda1"].snapshot_id
