8015f4ac0d33d94a645391fc5a118f61872878ab
Andrew Graham <andrew@firehawkvfx.com>
2021-04-25 20:28:38 +0930

    change sha1 ref

--- modules/terraform-aws-deadline-spot/main.tf
+++ modules/terraform-aws-deadline-spot/main.tf
@@ -59,7 +59,7 @@ resource "null_resource" "provision_deadline_spot" {
     config_template_sha1    = sha1(file(fileexists(local.override_config_template_file_path) ? local.override_config_template_file_path : local.config_template_file_path))
     deadline_spot_sha1      = sha1(file("${path.module}/ansible/collections/ansible_collections/firehawkvfx/deadline/deadline_spot.yaml"))
     deadline_spot_role_sha1 = sha1(file("${path.module}/ansible/collections/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/tasks/main.yml"))
-    deadline_roles_tf_sha1  = sha1(file("${path.module}/modules/deadline/main.tf"))
+    deadline_roles_tf_sha1  = sha1(local.instance_profile)
     # spot_access_key_id      = module.deadline.spot_access_key_id
     # spot_secret             = module.deadline.spot_secret
     volume_size = var.node_centos_volume_size
