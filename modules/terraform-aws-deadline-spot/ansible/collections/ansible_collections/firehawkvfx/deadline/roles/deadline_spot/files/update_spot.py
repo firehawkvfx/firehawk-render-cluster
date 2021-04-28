fb1e2c616e6ddbe9c5c63efef544bd4e4781addd
Andrew Graham <andrew@firehawkvfx.com>
2021-04-29 07:25:37 +0930

    update template path

--- modules/terraform-aws-deadline-spot/ansible/collections/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/files/update_spot.py
+++ modules/terraform-aws-deadline-spot/ansible/collections/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/files/update_spot.py
@@ -4,8 +4,15 @@ from Deadline.Scripting import ClientUtils, RepositoryUtils
 # execute with:
 # deadlinecommand -ExecuteScriptNoGui "update_spot.py"
 
+import argparse
+
 def __main__():
     try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--template-path', help='The path to your spot fleet template to configure deadline with')
+        args = parser.parse_args()
+        print('template path: {}'.format(args))
+
         with open('/home/ubuntu/config_generated.json') as json_file:
             configs = json.load(json_file)
             if not configs:
