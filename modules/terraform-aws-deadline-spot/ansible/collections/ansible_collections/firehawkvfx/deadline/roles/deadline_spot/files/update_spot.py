8b7c7dfa5e78925d9ba437d6308fbeea956a1409
Andrew Graham <andrew@firehawkvfx.com>
2021-04-28 22:45:12 +0930

    test main func

--- modules/terraform-aws-deadline-spot/ansible/collections/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/files/update_spot.py
+++ modules/terraform-aws-deadline-spot/ansible/collections/ansible_collections/firehawkvfx/deadline/roles/deadline_spot/files/update_spot.py
@@ -4,16 +4,16 @@ from Deadline.Scripting import ClientUtils, RepositoryUtils
 # execute with:
 # deadlinecommand -ExecuteScriptNoGui "update_spot.py"
 
+def __main__():
+    try:
+        with open('/home/ubuntu/config_generated.json') as json_file:
+            configs = json.load(json_file)
+            # config = str({"test":"test"})
+            # configs = json.loads(config)
+            if not configs:
+                raise Exception("No Spot Fleet Request Configuration found.")
 
-try:
-    with open('/home/ubuntu/config_generated.json') as json_file:
-        configs = json.load(json_file)
-        # config = str({"test":"test"})
-        # configs = json.loads(config)
-        if not configs:
-            raise Exception("No Spot Fleet Request Configuration found.")
-
-        RepositoryUtils.AddOrUpdateServerData("event.plugin.spot", "Config", json.dumps(configs))
-except Exception as e:
-    print(e)
-    raise e
+            RepositoryUtils.AddOrUpdateServerData("event.plugin.spot", "Config", json.dumps(configs))
+    except Exception as e:
+        print(e)
+        raise e
