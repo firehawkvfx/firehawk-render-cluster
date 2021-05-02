bdf3dd6b1ba8be31c45326b7f0b36d1b365f741d
Andrew Graham <andrew@firehawkvfx.com>
2021-05-02 13:26:58 +0930

    pass data as base 64

--- modules/terraform-aws-user-data-rendernode/outputs.tf
+++ modules/terraform-aws-user-data-rendernode/outputs.tf
@@ -1,6 +1,6 @@
-output "user_data" {
-  value = data.template_file.user_data_auth_client.rendered
-}
+# output "user_data" {
+#   value = data.template_file.user_data_auth_client.rendered
+# }
 
 output "user_data_base64" {
   value = base64encode(data.template_file.user_data_auth_client.rendered)
