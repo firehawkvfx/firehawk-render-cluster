737e4915050a68813597ef2450d5b123234fa524
Andrew Graham <queglay@gmail.com>
2021-06-05 12:36:01 +0930

    update fsx outputs

--- modules/terraform-aws-fsx/module/outputs.tf
+++ modules/terraform-aws-fsx/module/outputs.tf
@@ -1,3 +1,6 @@
+output "fsx_storage" {
+  value = aws_fsx_lustre_file_system.fsx_storage
+}
 output "network_interface_ids" {
   value = length(aws_fsx_lustre_file_system.fsx_storage) > 0 ? aws_fsx_lustre_file_system.fsx_storage.*.network_interface_ids : null
 }
@@ -23,5 +26,5 @@ output "fsx_private_ip" {
     data.aws_network_interface.fsx_primary_interface,
     aws_route53_record.fsx_record
   ]
-  value = local.fsx_private_ip
+  value = length(aws_fsx_lustre_file_system.fsx_storage) > 0 ? local.fsx_private_ip : null
 }
