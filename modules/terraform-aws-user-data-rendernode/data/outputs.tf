8b9004596b80545171f71644e267fedd8e1542d7
Andrew Graham <queglay@gmail.com>
2021-06-05 12:39:54 +0930

    update conditional output read for user data

--- modules/terraform-aws-user-data-rendernode/data/outputs.tf
+++ modules/terraform-aws-user-data-rendernode/data/outputs.tf
@@ -1,9 +1,12 @@
+output "fsx_storage" {
+  value = data.terraform_remote_state.fsx.outputs.fsx_storage
+}
 output "fsx_private_ip" {
-  value = data.terraform_remote_state.fsx.outputs.fsx_private_ip
+  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_private_ip : null
 }
 output "fsx_dns_name" {
-  value = data.terraform_remote_state.fsx.outputs.fsx_dns_name
+  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_dns_name : null
 }
 output "fsx_mount_name" {
-  value = data.terraform_remote_state.fsx.outputs.fsx_mount_name
+  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_mount_name : null
 }
