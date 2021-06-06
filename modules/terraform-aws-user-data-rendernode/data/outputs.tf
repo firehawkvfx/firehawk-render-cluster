11446ff6a15a1f22a6323344a1a2a175699602b6
Andrew Graham <queglay@gmail.com>
2021-06-06 12:23:39 +0930

    fix condition error

--- modules/terraform-aws-user-data-rendernode/data/outputs.tf
+++ modules/terraform-aws-user-data-rendernode/data/outputs.tf
@@ -15,8 +15,8 @@ output "nfs_file_gateway" {
   value = data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway
 }
 output "nfs_private_ip" {
-  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) ? data.terraform_remote_state.file_gateway.outputs.nfs_private_ip : null
+  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_private_ip : null
 }
 output "nfs_file_share_path" {
-  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) ? data.terraform_remote_state.file_gateway.outputs.nfs_file_share_path : null
+  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_file_share_path : null
 }
