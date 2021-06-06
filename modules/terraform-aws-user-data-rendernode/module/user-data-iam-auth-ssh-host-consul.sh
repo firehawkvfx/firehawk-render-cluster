5b2446a8efc39b09198ce655b3d5e17e1e5fd7dd
Andrew Graham <queglay@gmail.com>
2021-06-06 14:44:01 +0930

    Add scripts to lessen user data

--- modules/terraform-aws-user-data-rendernode/module/user-data-iam-auth-ssh-host-consul.sh
+++ modules/terraform-aws-user-data-rendernode/module/user-data-iam-auth-ssh-host-consul.sh
@@ -21,39 +21,17 @@ function has_yum {
 # These variables are passed in via Terraform template interpolation
 /opt/consul/bin/run-consul --client --cluster-tag-key "${consul_cluster_tag_key}" --cluster-tag-value "${consul_cluster_tag_value}"
 
-# A retry function that attempts to run a command a number of times and returns the output
-function retry {
-  local -r cmd="$1"
-  local -r description="$2"
-
-  for i in $(seq 1 30); do
-    log "$description"
-
-    # The boolean operations with the exit status are there to temporarily circumvent the "set -e" at the
-    # beginning of this script which exits the script immediatelly for error status while not losing the exit status code
-    output=$(eval "$cmd") && exit_status=0 || exit_status=$?
-    errors=$(echo "$output") | grep '^{' | jq -r .errors
-
-    log "$output"
-
-    if [[ $exit_status -eq 0 && -z "$errors" ]]; then
-      echo "$output"
-      return
-    fi
-    log "$description failed. Will sleep for 10 seconds and try again."
-    sleep 10
-  done;
-
-  log "$description failed after 30 attempts."
-  exit $exit_status
-}
+# clone helper scripts
+git clone https://github.com/firehawkvfx/aws-ec2-user-data-scripts.git /var/tmp/aws-ec2-user-data-scripts
+sudo chmod +x /var/tmp/aws-ec2-user-data-scripts/scripts/*
+mv /var/tmp/aws-ec2-user-data-scripts/scripts/* /usr/local/bin/.
 
 # If vault cli is installed we can also perform these operations with vault cli
 # The necessary environment variables have to be set
 
 export VAULT_ADDR=https://vault.service.consul:8200
 ### Vault Auth IAM Method CLI
-retry \
+/usr/local/bin/retry \
   "vault login --no-print -method=aws header_value=vault.service.consul role=${example_role_name}" \
   "Waiting for Vault login"
 
@@ -89,9 +67,6 @@ if test ! -f "/etc/ssh/ssh_host_rsa_key-cert.pub"; then
 fi
 chmod 0640 /etc/ssh/ssh_host_rsa_key-cert.pub
 
-# systemctl stop sshd 
-# log "LogLevel VERBOSE" | tee --append /etc/ssh/sshd_config # for debug
-
 # Private key and cert are both required for ssh to another host.  Multiple entries for host key may exist.
 grep -q "^HostKey /etc/ssh/ssh_host_rsa_key" /etc/ssh/sshd_config || echo 'HostKey /etc/ssh/ssh_host_rsa_key' | tee --append /etc/ssh/sshd_config
 
@@ -102,37 +77,7 @@ sed -i 's@HostCertificate.*@HostCertificate /etc/ssh/ssh_host_rsa_key-cert.pub@g
 # Add the CA cert to use it for known host verification # curl http://vault.service.consul:8200/v1/ssh-host-signer/public_key
 key="$(vault read -field=public_key ssh-host-signer/config/ca)"
 
-function ensure_known_hosts {
-  local -r ssh_known_hosts_path="$1"
-  local -r aws_external_domain=${aws_external_domain}
-
-  if test ! -f $ssh_known_hosts_path; then
-      log "Creating $ssh_known_hosts_path"
-      touch $ssh_known_hosts_path # ensure known hosts file exists
-  fi
-  if [[ "$OSTYPE" == "darwin"* ]]; then # Acquire file permissions.
-      octal_permissions=$(stat -f %A "$ssh_known_hosts_path" | rev | sed -E 's/^([[:digit:]]{4})([^[:space:]]+)/\1/' | rev ) # clip to 4 zeroes
-  else
-      octal_permissions=$(stat --format '%a' "$ssh_known_hosts_path" | rev | sed -E 's/^([[:digit:]]{4})([^[:space:]]+)/\1/' | rev) # clip to 4 zeroes
-  fi
-  octal_permissions=$( python3 -c "print( \"$octal_permissions\".zfill(4) )" ) # pad to 4 zeroes
-  log "$ssh_known_hosts_path octal_permissions currently $octal_permissions."
-  if [[ "$octal_permissions" != "0644" ]]; then
-      log "...Setting to 0644"
-      chmod 0644 $ssh_known_hosts_path
-  fi
-  if [[ -z "$aws_external_domain" ]]; then
-    # aws_external_domain var is empty, this must be an internal host, so we dont intend for this to connect to external hosts except by their internal DNS name
-    grep -q "^@cert-authority \*\.consul" $ssh_known_hosts_path || echo "@cert-authority *.consul" | tee --append $ssh_known_hosts_path
-    sed -i "s#@cert-authority \*\.consul.*#@cert-authority *.consul $key#g" $ssh_known_hosts_path
-  else
-    grep -q "^@cert-authority \*\.consul,\*\.$aws_external_domain" $ssh_known_hosts_path || echo "@cert-authority *.consul,*.$aws_external_domain" | tee --append $ssh_known_hosts_path
-    sed -i "s#@cert-authority \*\.consul,\*\.$aws_external_domain.*#@cert-authority *.consul,*.$aws_external_domain $key#g" $ssh_known_hosts_path
-  fi
-  ls -ltriah $ssh_known_hosts_path
-  log "Added CA to $ssh_known_hosts_path."
-}
-ensure_known_hosts /etc/ssh/ssh_known_hosts
+/usr/local/bin/ensure-known-hosts "/etc/ssh/ssh_known_hosts" "${aws_external_domain}" "$key"
 
 ### Finally allow users with signed client certs to login.
 # If TrustedUserCAKeys not defined, then add it to sshd_config
@@ -150,8 +95,4 @@ else # assume ubuntu
   systemctl restart systemd-networkd
 fi
 
-# log "Signing SSH host key done. Revoking vault token..."
 vault token revoke -self
-
-# if this script fails, we can set the instance health status but we need to capture a fault
-# aws autoscaling set-instance-health --instance-id i-0b03e12682e74746e --health-status Unhealthy
--- modules/terraform-aws-user-data-rendernode/module/user-data-install-deadline-worker-cert.sh
+++ modules/terraform-aws-user-data-rendernode/module/user-data-install-deadline-worker-cert.sh
@@ -32,70 +32,6 @@ function has_yum {
 function has_apt_get {
   [[ -n "$(command -v apt-get)" ]]
 }
-# A retry function that attempts to run a command a number of times and returns the output
-function retry {
-  local -r cmd="$1"
-  local -r description="$2"
-  attempts=5
-
-  for i in $(seq 1 $attempts); do
-    log "$description"
-
-    # The boolean operations with the exit status are there to temporarily circumvent the "set -e" at the
-    # beginning of this script which exits the script immediatelly for error status while not losing the exit status code
-    output=$(eval "$cmd") && exit_status=0 || exit_status=$?
-    errors=$(echo "$output") | grep '^{' | jq -r .errors
-
-    log "$output"
-
-    if [[ $exit_status -eq 0 && -z "$errors" ]]; then
-      echo "$output"
-      return
-    fi
-    log "$description failed. Will sleep for 10 seconds and try again."
-    sleep 10
-  done;
-
-  log "$description failed after $attempts attempts."
-  exit $exit_status
-}
-function retrieve_file {
-  local -r source_path="$1"
-  if [[ -z "$2" ]]; then
-    local -r target_path="$source_path"
-  else
-    local -r target_path="$2"
-  fi
-  echo "Aquiring vault data... $source_path to $target_path"
-  response=$(retry \
-  "vault kv get -field=value $source_path/file" \
-  "Trying to read secret from vault")
-
-  echo "mkdir: $(dirname $target_path)"
-  mkdir -p "$(dirname $target_path)" # ensure the directory exists
-  echo "Check file path is writable: $target_path"
-  if test -f "$target_path"; then
-    echo "File exists: ensuring it is writeable"
-    chmod u+w "$target_path"
-    touch "$target_path"
-  else
-    echo "Ensuring path is writeable"
-    touch "$target_path"
-    chmod u+w "$target_path"
-  fi
-  if [[ -f "$target_path" ]]; then
-    chmod u+w "$target_path"
-  else
-    echo "Error: path does not exist, var may not be a file: $target_path "
-  fi
-  echo "Write file content: single operation"
-  echo "$response" | base64 --decode > $target_path
-  if [[ ! -f "$target_path" ]] || [[ -z "$(cat $target_path)" ]]; then
-    echo "Error: no file or empty result at $target_path"
-    exit 1
-  fi
-  echo "retrival done."
-}
 
 ### Centos 7 fix: Failed dns lookup can cause sudo commands to slowdown
 if $(has_yum); then
@@ -106,32 +42,7 @@ if $(has_yum); then
 fi
 
 ### Create deadlineuser
-function add_sudo_user() {
-  local -r user_name="$1"
-  if $(has_apt_get); then
-    sudo_group=sudo
-  elif $(has_yum); then
-    sudo_group=wheel
-  else
-    echo "ERROR: Could not find apt-get or yum."
-    exit 1
-  fi
-  echo "Ensuring user exists: $user_name with groups: $sudo_group $user_name"
-  if id "$user_name" &>/dev/null; then
-    echo 'User found.  Ensuring user is in sudoers.'
-    sudo usermod -a -G $sudo_group $user_name
-  else
-      echo 'user not found'
-      sudo useradd -m -d /home/$user_name/ -s /bin/bash -G $sudo_group $user_name
-  fi
-  echo "Adding user as passwordless sudoer."
-  touch "/etc/sudoers.d/98_$user_name"; grep -qxF "$user_name ALL=(ALL) NOPASSWD:ALL" /etc/sudoers.d/98_$user_name || echo "$user_name ALL=(ALL) NOPASSWD:ALL" >> "/etc/sudoers.d/98_$user_name"
-  sudo -i -u $user_name mkdir -p /home/$user_name/.ssh
-  # Generate a public and private key - some tools can fail without one.
-  rm -frv /home/$user_name/.ssh/id_rsa*
-  sudo -i -u $user_name bash -c "ssh-keygen -q -b 2048 -t rsa -f /home/$user_name/.ssh/id_rsa -C \"\" -N \"\""  
-}
-add_sudo_user $deadlineuser_name
+/usr/local/bin/add-sudo-user $deadlineuser_name
 
 printf "\n...Waiting for consul deadlinedb service before attempting to retrieve Deadline remote cert.\n\n"
 
@@ -143,12 +54,12 @@ chown $deadlineuser_name:$deadlineuser_name /opt/Thinkbox/
 chown $deadlineuser_name:$deadlineuser_name /opt/Thinkbox/certs/
 
 ### Vault Auth IAM Method CLI
-retry \
+/usr/local/bin/retry \
   "vault login --no-print -method=aws header_value=vault.service.consul role=${example_role_name}" \
   "Waiting for Vault login"
 
 # Retrieve previously generated secrets from Vault.  Would be better if we can use vault as an intermediary to generate certs.
-retrieve_file "$client_cert_vault_path" "$client_cert_file_path"
+/usr/local/bin/retrieve-vault-file "$client_cert_vault_path" "$client_cert_file_path"
 echo "Finalise permissions"
 chown $deadlineuser_name:$deadlineuser_name $client_cert_file_path
 chmod u=rw,g=rw,o-rwx $client_cert_file_path
