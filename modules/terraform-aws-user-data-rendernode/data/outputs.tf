a35bb90812b1dcc60f80562c9849a266bcf44210
Andrew Graham <queglay@gmail.com>
2021-06-13 14:37:28 +0930

    handle non existent bucket

--- modules/terraform-aws-user-data-rendernode/data/outputs.tf
+++ modules/terraform-aws-user-data-rendernode/data/outputs.tf
@@ -2,21 +2,21 @@ output "fsx_storage" {
   value = try(data.terraform_remote_state.fsx.outputs.fsx_storage, null)
 }
 output "fsx_private_ip" {
-  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_private_ip : null
+  value = length( try(data.terraform_remote_state.fsx.outputs.fsx_storage,[]) ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_private_ip : null
 }
 output "fsx_dns_name" {
-  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_dns_name : null
+  value = length( try(data.terraform_remote_state.fsx.outputs.fsx_storage,[]) ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_dns_name : null
 }
 output "fsx_mount_name" {
-  value = length( data.terraform_remote_state.fsx.outputs.fsx_storage ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_mount_name : null
+  value = length( try(data.terraform_remote_state.fsx.outputs.fsx_storage,[]) ) > 0 ? data.terraform_remote_state.fsx.outputs.fsx_mount_name : null
 }
 
 output "nfs_file_gateway" {
   value = data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway
 }
 output "nfs_private_ip" {
-  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_private_ip : null
+  value = length( try(data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway,[]) ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_private_ip : null
 }
 output "nfs_file_share_path" {
-  value = length( data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_file_share_path : null
+  value = length( try(data.terraform_remote_state.file_gateway.outputs.nfs_file_gateway,[]) ) > 0 ? data.terraform_remote_state.file_gateway.outputs.nfs_file_share_path : null
 }
