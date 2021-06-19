74f8b2a607450c033ef1f774d5ae0f2cdab5c2d5
Andrew Graham <queglay@gmail.com>
2021-06-19 16:45:26 +0930

    update typo

--- modules/terraform-aws-sqs-deadline-client/data/data.tf
+++ modules/terraform-aws-sqs-deadline-client/data/data.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
-data "terraform_remote_state" "terraform_aws_sqs_vpn" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
+data "terraform_remote_state" "terraform_aws_sqs_deadline_cert" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
   backend = "s3"
   config = {
     bucket = "state.terraform.${var.bucket_extension_vault}"
