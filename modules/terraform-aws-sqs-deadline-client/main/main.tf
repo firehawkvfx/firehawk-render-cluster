dba62ca62e3ea78ed8a273268b1a4304dd84645b
Andrew Graham <queglay@gmail.com>
2021-06-20 20:25:10 +0930

    update send deadline payload

--- modules/terraform-aws-sqs-deadline-client/main/main.tf
+++ modules/terraform-aws-sqs-deadline-client/main/main.tf
@@ -12,7 +12,10 @@ resource "null_resource" "sqs_notify" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-      ${path.module}/modules/firehawk-auth-scripts/sqs-notify --resourcetier "${var.resourcetier}" --sqs-queue-url "${var.sqs_remote_in_deadline_cert_url}" --host1 "${var.host1}" --host2 "${var.host2}" --token-policy deadline_client --token-use-limit "${var.token_use_limit}"
+      ${path.module}/modules/firehawk-auth-scripts/sqs-send-deadline-payload --resourcetier "${var.resourcetier}"
 EOT
   }
 }
+
+# below was deprecated in favor of the simpler standardised command above.
+# ${path.module}/modules/firehawk-auth-scripts/sqs-notify --resourcetier "${var.resourcetier}" --sqs-queue-url "${var.sqs_remote_in_deadline_cert_url}" --host1 "${var.host1}" --host2 "${var.host2}" --token-policy deadline_client --token-use-limit "${var.token_use_limit}"
