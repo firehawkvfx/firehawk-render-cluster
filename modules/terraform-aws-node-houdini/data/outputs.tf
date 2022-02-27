9f38b2fa9ef9e261e10522bbf9e7f5be36fd7598
Andrew Graham <queglay@gmail.com>
2022-02-28 00:07:03 +1030

    add try to data output

--- modules/terraform-aws-node-houdini/data/outputs.tf
+++ modules/terraform-aws-node-houdini/data/outputs.tf
@@ -2,8 +2,8 @@ output "user_data_base64" {
   value = try(data.terraform_remote_state.user_data.outputs.user_data_base64, null)
 }
 output "rendervpc_id" {
-  value = aws_vpc.rendervpc.id
+  value = try(data.aws_vpc.rendervpc.id, null)
 }
 output "vaultvpc_id" {
-  value = aws_vpc.vaultvpc.id
+  value = try(data.aws_vpc.vaultvpc.id, null)
 }
