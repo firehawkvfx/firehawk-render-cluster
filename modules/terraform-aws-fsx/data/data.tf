7fa107317d1426f269329df217067ec1f2dc1b73
Andrew Graham <queglay@gmail.com>
2022-02-28 21:55:51 +1030

    Allow data to handle non existance of vpc

--- modules/terraform-aws-fsx/data/data.tf
+++ modules/terraform-aws-fsx/data/data.tf
@@ -9,23 +9,34 @@ data "aws_ssm_parameter" "cloud_fsx_storage" {
 
 data "aws_region" "current" {}
 
+data "terraform_remote_state" "rendervpc" {
+  backend = "s3"
+  config = {
+    bucket = "state.terraform.${var.bucket_extension}"
+    key    = "firehawk-render-cluster/modules/terraform-aws-render-vpc/terraform.tfstate"
+    region = data.aws_region.current.name
+  }
+}
 data "aws_vpc" "rendervpc" {
+  count = length(data.terraform_remote_state.rendervpc.outputs.vpc_id) > 0 ? 1 : 0
   default = false
-  tags    = var.common_tags_rendervpc
+  id = data.terraform_remote_state.rendervpc.outputs.vpc_id
+  # tags    = var.common_tags_rendervpc
 }
 data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.rendervpc.id
+  count = length(data.aws_vpc.rendervpc) > 0 ? 1 : 0
+  vpc_id = data.aws_vpc.rendervpc[0].id
   tags   = tomap({ "area" : "private" })
 }
 data "aws_subnet" "private" {
-  for_each = data.aws_subnet_ids.private.ids
+  for_each = length(data.aws_subnet_ids.private) > 0 ? data.aws_subnet_ids.private[0].ids : []
   id       = each.value
 }
 output "vpc_id" {
-  value = data.aws_vpc.rendervpc.id
+  value = data.aws_vpc.rendervpc[0].id
 }
 output "rendervpc_cidr" {
-  value = data.aws_vpc.rendervpc.cidr_block
+  value = data.aws_vpc.rendervpc[0].cidr_block
 }
 output "private_subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.private : s.cidr_block]
