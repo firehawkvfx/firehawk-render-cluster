5a8eccf18bd19566950246f5390ca356cb3b98f5
Andrew Graham <queglay@gmail.com>
2022-03-01 22:07:39 +1030

    handle conditional vpc

--- modules/terraform-aws-render-vpc-vault-vpc-peering/variables.tf
+++ modules/terraform-aws-render-vpc-vault-vpc-peering/variables.tf
@@ -35,3 +35,12 @@ variable "vpcname_vaultvpc" {
   description = "The VPC name containing Vault"
   type = string
 }
+variable "bucket_extension" {
+  description = "The extension for cloud storage used to label your S3 storage buckets (eg: example.com, my-name-at-gmail.com). This can be any unique name (it must not be taken already, globally).  commonly, it is a domain name you own, or an abbreviated email adress.  No @ symbols are allowed. See this doc for naming restrictions on s3 buckets - https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html"
+  type        = string
+}
+
+variable "bucket_extension_vault" {
+    description = "The bucket extension where the terraform remote state resides"
+    type = string
+}
