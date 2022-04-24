6332f44094f1314092addbd32ba9ee61ed3832f4
Andrew Graham <queglay@gmail.com>
2022-04-24 16:09:57 +0930

    Relocate deploy vpc to predeploy from init

--- modules/terraform-bastion-graphical/main.tf
+++ modules/terraform-bastion-graphical/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "provisioner_security_group" { # read the arn with
   backend = "s3"
   config = {
     bucket = "state.terraform.${var.bucket_extension}"
-    key    = "init/modules/terraform-aws-sg-provisioner/terraform.tfstate"
+    key    = "predeploy/modules/terraform-aws-sg-provisioner/terraform.tfstate"
     region = data.aws_region.current.name
   }
 }
